5b150d64291fa7c6753d4624ea453f53401109a2
g1tka <luckycookiedaisukii@gmail.com>
2024-09-21 20:31:29 +0900

    Study prevent form error

--- chapter3/user-output.php
+++ chapter3/user-output.php
@@ -1,6 +1,9 @@
 <?php require '../header.php'; ?>
 <?php
-echo 'ようこそ、', $_REQUEST['user'], 'さん。';
+// echo 'ようこそ、', $_REQUEST['user'], 'さん。';
+if (isset($_REQUEST['user'])) {
+    echo 'ようこそ、', htmlspecialchars($_REQUEST['user']), 'さん。'; 
+}
 ?>
 <?php require '../footer.php'; ?>
 
@@ -23,5 +26,27 @@ $_REQUEST['user']これでよびだしできるが、
 $_REQUESTが変数のようなもの（変数の一種といえる。）つまり箱。
 echoで表示。
 つなげて表示するために'', '', ''と連結して表示している。
+
+http://localhost/php/chapter3/user-output.php
+ここに直接アクセスした場合以下のように表示される（エラー）。
+ようこそ、
+Warning: Undefined array key "user" in C:\xampp\htdocs\php\chapter3\user-output.php on line 3
+さん。
+
+入力しなかった場合でif文を作成し対応する。
+if (isset($_REQUEST['user'])) {
+  ここに実行したいコード
+}
+※isset() 変数が定義されているかどうかを調べる変数
+
+もう一つ懸念。
+入力フォームに<h1>なまえ</h1>というようにHTMLタグを入力された場合。
+h1タグが反映され大きい文字で表示される。困ります。
+無効にするには。
+
+echo 'ようこそ、', htmlspecialchars($_REQUEST['user]), 'さん。';
+
+※htmlspecialchars() 特別な働きをする文字について無効にする。
+ユーザーが入力した文字を表示したり保存したりする際に安全性を高める。
 
 -->
