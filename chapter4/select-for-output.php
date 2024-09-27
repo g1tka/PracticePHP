6620103689e2891a55a817eb5118a1d057f4303b
g1tka <luckycookiedaisukii@gmail.com>
2024-09-27 21:49:21 +0900

    Study foreach

--- chapter4/select-for-output.php
+++ chapter4/select-for-output.php
@@ -8,4 +8,21 @@ echo $_REQUEST['count'], '個の商品をカートに追加しました。';
 http://localhost/php/chapter4/select-for-input.php
 
 セレクトボックスから選択させる方式のフォーム！
+<form action="select-for-output.php" method="post">
+<select name="count">
+<option value="0">0</option>
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+<option value="6">6</option>
+<option value="7">7</option>
+<option value="8">8</option>
+<option value="9">9</option>
+</select>
+<p><input type="submit" value="確定"></p>
+</form>
+
+
 -->
