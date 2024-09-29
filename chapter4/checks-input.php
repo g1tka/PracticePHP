f97f7732e0ba8c3294a4ace024cd13632702aec0
g1tka <luckycookiedaisukii@gmail.com>
2024-09-29 22:23:24 +0900

    Study checkbox

--- chapter4/checks-input.php
+++ chapter4/checks-input.php
@@ -13,3 +13,27 @@ foreach ($genre as $item) {
 <p><input type="submit" value="確定"></p>
 </form>
 <?php require '../footer.php'; ?>
+
+<!--
+input の type="checkbox"でチェックボックス。
+name="genre"の配列設置
+value(値)="カメラとかパソコンとか"
+複数設置したい
+foreachループを使って、配列使って、複数設置
+foreach ($genre as $item)
+foreach (配列　as  値（前回はキー）)
+$itemはジャンルから値を取り出す変数として定義
+
+チェックボックスと選択肢となる文字列が書いてある。
+チェックボックスのコードについて。
+type="checkbox": この入力がチェックボックスであることを指定します。
+name="genre[]": name属性にgenre[]を設定して、複数のチェックボックスが
+選択された場合に配列として送信されることを示しています。
+[]は配列であることを示す記法です。
+※配列genreが存在しない場合の場合分けが必要。
+value="', $item, '": 各チェックボックスのvalue属性に、
+ループで取得した$item（商品ジャンルの名前）が設定されます。
+これにより、チェックボックスが選択されたときに送信される値が決まります。
+
+echo '<input type="checkbox" name="genre[]" value="', $item, '">';
+-->
--- chapter4/checks-output.php
+++ chapter4/checks-output.php
@@ -1,8 +1,46 @@
 <?php require '../header.php'; ?>
 <?php
-foreach ($_REQUEST['genre'] as $item) {
-	echo '<p>', $item, '</p>';
+if (isset($_REQUEST['genre'])) {
+
+	foreach ($_REQUEST['genre'] as $item) {
+		echo '<p>', $item, '</p>';
+	}
+	echo 'に関するお買い得情報をお送りさせて頂きます。';
 }
-echo 'に関するお買い得情報をお送りさせて頂きます。';
+else {
+	echo '何も選択されていません。';
+}
 ?>
 <?php require '../footer.php'; ?>
+
+<!--
+$genre=['カメラ', 'パソコン', '時計', '家電', '書籍', '文房具', '食品'];
+foreach ($genre as $item) {
+	echo '<p>';
+	echo '<input type="checkbox" name="genre[]" value="', $item, '">';
+	echo $item;
+	echo '</p>';
+}
+
+$_REQUEST['genre']でチェックされた配列genre（リクエストパラメータ）取得。
+ただこれは配列になっている。チェックしたものの文字列だけ欲しい。
+欲しいものはチェックボックスのvalue属性に設定した文字列。
+テレビ
+パソコン
+というように、順に表示したい。<p>で囲ったわけ。
+<p> $item </p>　こうしたいから、以下のように書く。
+'<p>', $item, '</p>'
+
+インプット画面について。
+チェックボックスと選択肢となる文字列が書いてある。
+チェックボックスのコードについて。
+type="checkbox": この入力がチェックボックスであることを指定します。
+
+name="genre[]": name属性にgenre[]を設定して、複数のチェックボックスが
+選択された場合に配列として送信されることを示しています。
+
+[]は配列であることを示す記法です。
+value="', $item, '": 各チェックボックスのvalue属性に、
+ループで取得した$item（商品ジャンルの名前）が設定されます。
+これにより、チェックボックスが選択されたときに送信される値が決まります。
+-->
