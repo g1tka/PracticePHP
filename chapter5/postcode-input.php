b6ce54f27206290bd13af04642d279ecf2c65066
g1tka <luckycookiedaisukii@gmail.com>
2024-10-02 21:31:59 +0900

    Study preg match

--- chapter5/postcode-input.php
+++ chapter5/postcode-input.php
@@ -5,3 +5,11 @@
 <input type="submit" value="確定">
 </form>
 <?php require '../footer.php'; ?>
+
+<!--
+type="text"で好きな文字を入れられる。
+しかしoutput.phpの方で判定する際に、半角数字のみ判定するようにしているので、
+全角数字ではfalseになってしまう。
+（正規表現の設定の仕方）
+
+-->
