4a6aabe89554a31a9724836a2464eb624bc69b28
g1tka <luckycookiedaisukii@gmail.com>
2024-10-04 21:45:49 +0900

    Study password type

--- chapter5/password-input.php
+++ chapter5/password-input.php
@@ -6,3 +6,23 @@
 <input type="submit" value="確定">
 </form>
 <?php require '../footer.php'; ?>
+
+<!--
+type="password"にすると、パスワード入力欄になる。
+これは、入力した文字列が表示されなくなる特徴あり。
+
+$password=$_REQUEST['password'];
+if (preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$/', 
+	$password)) {
+	echo 'パスワード「', $password, '」を確認しました。';
+} else {
+	echo 'パスワード「', $password, '」は適切ではありません。';
+}
+
+正規表現が少し複雑。
+(?=.*[a-z])英語小文字を含む
+(?=.*[A-Z])英語大文字を含む
+(?=.*[0-9])数字を含む
+[a-zA-Z0-9]いずれか一文字ふくむ
+{8,}直前の文字が8文字以上続く
+-->
