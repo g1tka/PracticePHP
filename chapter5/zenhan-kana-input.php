0bcde820f2763bfa01e2cfeaaa17ff0c21c8a86f
g1tka <luckycookiedaisukii@gmail.com>
2024-10-05 20:41:08 +0900

    Study convert kana

--- chapter5/zenhan-kana-input.php
+++ chapter5/zenhan-kana-input.php
@@ -5,3 +5,15 @@
 <input type="submit" value="確定">
 </form>
 <?php require '../footer.php'; ?>
+
+<!--
+テキストボックスに日本語入力させる。
+半角入力されても全角。
+全角なら全角のまま。
+
+mb_convert_kana関数
+mb_convert_kana(文字列)
+
+例えば二文字になってしまうﾋﾟもピ一文字になる。
+
+-->
